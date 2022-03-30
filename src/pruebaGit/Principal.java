1c2ec5361b23918c2a81a759fa708ac5c00b2b5c
amjyebenes <amjyebenes@gmail.com>
2022-03-30 10:00:57 +0200

    en la rama cambio1, añado paquete y clase, modifico clase anterior

--- src/pruebaGit/Principal.java
+++ src/pruebaGit/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
 		System.out.println("segundo syso");
-
+		System.out.println("añado rama cambio1");
 	}
 
 }
